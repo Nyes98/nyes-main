7320cd400326b9f9ad84f9c8b731e55a9b341dd9
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2022-10-04 10:41:27 +0900

    1004 수업 추가

--- imsi/crane.js
+++ imsi/crane.js
@@ -1,28 +1,28 @@
 function solution(board, moves) {
   moves = moves.map((item) => item - 1);
   var answerCnt = 0;
-  let tempArr = [];
-  let tempArr2 = [];
+  let rowArr = [];
+  let colArr = [];
   let answerArr = [];
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board.length; j++) {
-      tempArr.push(board[j][i]);
-      if (tempArr.length == board.length) {
-        tempArr2.push(tempArr);
-        tempArr = [];
+      rowArr.push(board[j][i]);
+      if (rowArr.length == board.length) {
+        colArr.push(rowArr);
+        rowArr = [];
       }
     }
   }
 
   for (let i = 0; i < moves.length; i++) {
     for (let j = 0; j < board.length; j++) {
-      let curItem = tempArr2[moves[i]][j];
+      let curItem = colArr[moves[i]][j];
       if (curItem == 0) {
         continue;
       } else {
         answerArr.push(curItem);
-        tempArr2[moves[i]][j] = 0;
+        colArr[moves[i]][j] = 0;
 
         if (
           answerArr.length >= 2 &&
