4e80b8b620622d52f7e0c28d315167ca2b29c8a3
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2022-11-01 21:38:02 +0900

    1101 class add
    socket

---
+++ 1101_38/socketio/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const session = require("express-session");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const socket = require("./socket.js");
+// const ws = require("ws");
+
+const app = express();
+dotenv.config();
+
+app.set("port", process.env.PORT || 8080);
+
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === "production") morgan("combined")(req, res, next);
+  else morgan("dev")(req, res, next);
+});
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "asdf",
+  })
+);
+
+app.get("/test", (req, res) => {
+  console.log(req.query);
+});
+
+const server = app.listen(app.get("port"), () => {
+  console.log("서버오픈");
+});
+
+socket(server);
