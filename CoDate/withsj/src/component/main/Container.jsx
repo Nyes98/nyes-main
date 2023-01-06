178963e428e10d6e1fdb2dd28fc808e9faeca293
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2023-01-06 17:39:12 +0900

    0106 interface

--- CoDate/withsj/src/component/main/Container.jsx
+++ CoDate/withsj/src/component/main/Container.jsx
@@ -6,83 +6,82 @@ import SHA256 from "crypto-js/sha256";
 export default function MainContainer() {
   const [_data, setData] = useState("");
   const [level, setLevel] = useState(0);
-  const [nonce, setNonce] = useState(0);
+  const [nonce, setNonce] = useState(1);
   const [header, setHeader] = useState([]);
   const [block, setBlock] = useState([]);
-  const [auto, setAuto] = useState("false");
-  const [event, setEvent] = useState("false");
+  const [auto, setAuto] = useState(false);
+  const [event, setEvent] = useState(false);
 
   const nonceClick = () => {
-    setEvent(!event);
+    for (let i = 0; i < 500; i++) {
+      setNonce(+nonce + 1);
+      console.log(nonce);
+      if (nonce == 100) break;
+    }
   };
 
-  // useEffect(() => {
-  //   while (event) {
-  //     setNonce(nonce + 1);
-  //   }
-  //   console.log(nonce);
-  // }, [event]);
+  useEffect(() => {}, [nonce]);
 
-  // class Header {
-  //   constructor(_height, _previousHash) {
-  //     this.version = Header.getVersion();
-  //     this.height = _height;
-  //     this.timeStamp = Header.getTimeStamp();
-  //     this.previousHash = _previousHash || "0".repeat(64);
-  //     this.nonce = nonce;
-  //   }
+  class Header {
+    constructor(_height, _previousHash) {
+      this.version = Header.getVersion();
+      this.height = _height;
+      this.timeStamp = Header.getTimeStamp();
+      this.previousHash = _previousHash || "0".repeat(64);
+      this.nonce = nonce;
+    }
 
-  //   static getVersion() {
-  //     return "1.0.0";
-  //   }
-  //   static getTimeStamp() {
-  //     return Date.now();
-  //   }
-  // }
+    static getVersion() {
+      return "1.0.0";
+    }
+    static getTimeStamp() {
+      return Date.now();
+    }
+  }
 
-  // class Block {
-  //   constructor(_header, _data) {
-  //     this.version = _header.version;
-  //     this.height = _header.height;
-  //     this.timeStamp = _header.timeStamp;
-  //     this.previousHash = _header.previousHash;
-  //     this.merkleRoot = Block.getMerkleRoot(_data);
-  //     this.hash = Block.createBlockHash(header, Block.getMerkleRoot(_data));
-  //     this.data = _data;
-  //   }
+  class Block {
+    constructor(_header, _data) {
+      this.version = _header.version;
+      this.height = _header.height;
+      this.timeStamp = _header.timeStamp;
+      this.previousHash = _header.previousHash;
+      this.merkleRoot = Block.getMerkleRoot(_data);
+      this.hash = Block.createBlockHash(header, Block.getMerkleRoot(_data));
+      this.data = _data;
+    }
 
-  //   static getMerkleRoot(_data) {
-  //     if (!Array.isArray(_data)) return "not Array";
+    static getMerkleRoot(_data) {
+      if (!Array.isArray(_data)) return "not Array";
 
-  //     let merkleArr = _data.map((item) =>
-  //       SHA256(item).toString().toUpperCase()
-  //     );
+      let merkleArr = _data.map((item) =>
+        SHA256(item).toString().toUpperCase()
+      );
 
-  //     while (merkleArr.length > 1) {
-  //       const tempArr = [];
-  //       for (let i = 0; i < merkleArr.length; i += 2) {
-  //         if (i + 1 === merkleArr.length) {
-  //           tempArr.push(merkleArr[i]);
-  //         } else {
-  //           tempArr.push(
-  //             SHA256(merkleArr[i] + merkleArr[i + 1])
-  //               .toString()
-  //               .toUpperCase()
-  //           );
-  //         }
-  //       }
-  //       merkleArr = tempArr;
-  //     }
-  //     return merkleArr[0];
-  //   }
+      while (merkleArr.length > 1) {
+        const tempArr = [];
+        for (let i = 0; i < merkleArr.length; i += 2) {
+          if (i + 1 === merkleArr.length) {
+            tempArr.push(merkleArr[i]);
+          } else {
+            tempArr.push(
+              SHA256(merkleArr[i] + merkleArr[i + 1])
+                .toString()
+                .toUpperCase()
+            );
+          }
+        }
+        merkleArr = tempArr;
+      }
+      return merkleArr[0];
+    }
 
-  //   static createBlockHash(_header, _merkleRoot) {
-  //     const values = Object.values(_header);
-  //     const data = values.join("") + _merkleRoot;
-  //     // console.log(SHA256(data).toString().toUpperCase());
-  //     return SHA256(data).toString().toUpperCase();
-  //   }
-  // }
+    static createBlockHash(_header, _merkleRoot) {
+      const values = Object.values(_header);
+      const data = values.join("") + _merkleRoot;
+      // console.log(SHA256(data).toString().toUpperCase());
+      return SHA256(data).toString().toUpperCase();
+    }
+  }
 
   return (
     <MainComponent setData={setData} nonceClick={nonceClick} nonce={nonce} />
