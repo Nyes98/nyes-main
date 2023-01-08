acb25f2b1898ea6d602110fc32ac6ad0b0e51a3a
nyes <pys9839@gmail.com>
2023-01-08 22:36:12 +0900

    230108 overlap file remove

--- CoDate/withsj/src/App.js
+++
@@ -1,8 +0,0 @@
-import MainContainer from "./component/main/Container";
-import "./App.css";
-
-function App() {
-  return <MainContainer></MainContainer>;
-}
-
-export default App;
--- btp/drop/src/App.js
+++
@@ -1,7 +0,0 @@
-import DropdownContainer from "./components/Dropdown/Container";
-
-const App = () => {
-  return <DropdownContainer></DropdownContainer>;
-};
-
-export default App;
