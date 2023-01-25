9e54212761cf9aecf0400efebb86d1ea45976b42
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2023-01-25 17:03:14 +0900

    0125 typescript

--- programmers/knight.js
+++ programmers/knight.js
@@ -1,13 +1,49 @@
+// function solution(number, limit, power) {
+//   var answer = 0;
+//   let arr = [];
+
+//   for (let i = 1; i <= number; i++) {
+//     let cnt = 0;
+
+//     for (let j = 1; j <= i; j++) {
+//       if (i % j == 0) cnt++;
+//       if (j == i) arr.push(cnt);
+//     }
+//   }
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > limit) answer += power;
+//     else answer += arr[i];
+//   }
+
+//   console.log(answer);
+//   return answer;
+// }
+
+// solution(100000, 3, 2);
+
 function solution(number, limit, power) {
   var answer = 0;
   let arr = [];
 
   for (let i = 1; i <= number; i++) {
     let cnt = 0;
+    for (let j = 1; j <= Math.sqrt(i); j++) {
+      console.log("i", i);
+      if (i % j == 0) {
+        console.log("j", j);
+        cnt++;
+        console.log("제곱근 약수추가", cnt);
 
-    for (let j = 1; j <= i; j++) {
-      if (i % j == 0) cnt++;
-      if (j == i) arr.push(cnt);
+        if (i / j != j) {
+          console.log("넘나제", i / j);
+          cnt++;
+          console.log("제곱근으로 나눈 수 약수추가", cnt);
+        }
+      }
+
+      if (j == parseInt(Math.sqrt(i))) arr.push(cnt);
+      console.log(arr);
     }
   }
 
@@ -15,9 +51,8 @@ function solution(number, limit, power) {
     if (arr[i] > limit) answer += power;
     else answer += arr[i];
   }
-
   console.log(answer);
   return answer;
 }
 
-solution(100000, 3, 2);
+solution(10, 3, 2);
