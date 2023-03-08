7ff38d0d6f3b00205bd10de5a3052621a4296383
nyes <pys9839@gmail.com>
2023-03-08 23:22:13 +0900

    tsprac

--- programmers/dot.js
+++ programmers/dot.js
@@ -5,15 +5,9 @@ function solution(k, d) {
     answer += parseInt(parseInt(Math.sqrt(d ** 2 - (i * k) ** 2)) / k) + 1;
     console.log("i번째", 0);
     console.log("몇개", parseInt(Math.sqrt(d ** 2 - (i * k) ** 2)) / k + 1);
-    // for (let j = 0; j <= parseInt(d / k); j++) {
-    //   if ((i * k) ** 2 + (j * k) ** 2 > d ** 2) break;
-    //   answer++;
-    // }
   }
-  //   answer += d + 1;
   console.log("최종", answer);
   return answer;
 }
 
 solution(1, 5);
-// solution(2, 4);
